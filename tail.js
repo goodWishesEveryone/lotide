5d1cd01c01a2be4bf85186bbd577e843baa33266
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-11-23 02:08:55 -0500

    Modified tail.js

--- tail.js
+++ tail.js
@@ -5,8 +5,10 @@ const tail = function(inputArr) {
   // remove the head or first elements from the array, thus tailArray contains the tail only -- that is, everything except for the first item or head of the provided array.
   tailArray = inputArr.slice(1);
   if (inputArr.length === 0 || inputArr.length === 1) {
-    return console.log(tailArray);
-  } else return console.log(tailArray);
+    return tailArray = [];
+  } else
+  
+    return tailArray;
 };
 
 const assertEqual = function(actual, expected) {
@@ -20,17 +22,35 @@ const assertEqual = function(actual, expected) {
 
 
 // TEST CODEs:
-// Check the original array, it shouldn't be modified
 
-let words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// TEST CASE 1: Check the returned array elements
+let result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);         // ensure we get back two elements
+assertEqual(result[0], "Lighthouse");  // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");        // ensure second element is "Labs"
 
-//An array with only one element as well as an empty array should yield an empty array for its tail.
-let words1 = ["Labs"];
-tail(words1);
-assertEqual(words1.length, 1); // original array should still have 1 elements!
+
+// TEST CASE 2: An array with only one element should yield an empty array for its tail, thus array length is 0
+let result1 = tail(["Lighthouse"]);
+assertEqual(result1.length, 0);        // ensure we get back 0 elements
+assertEqual(result1[0], undefined);    // ensure first element is "undefined"
+
+
+// TEST CASE 3: An empty array should yield an empty array for its tail, thus array length is 0
+let result2 = [];
+console.log(tail(result2));           // results an empty array
+assertEqual(result2.length, 0);       // ensure we get back 0 elements
+assertEqual(result2[0], undefined);   // ensure first element is "undefined"
+
+// TEST CASE 4: Checking the original array that it shouldn't be modified.
+let words = ["Peace", "Lighthouse"];
+console.log(tail(words));             // return "Lighthouse" as tail
+assertEqual(words.length, 2);         // original array should still have 2 elements!
+
+let words1 = ["Light"];
+console.log(tail(words1));            // return empty as tail
+assertEqual(words1.length, 1);        // original array should still have 1 elements!
 
 let words2 = [];
-tail(words2);
-assertEqual(words2.length, 0); // original array should still have 0 elements!
+console.log(tail(words2));            // return empty as tail
+assertEqual(words2.length, 0);        // original array is empty, thus 0 elements!
