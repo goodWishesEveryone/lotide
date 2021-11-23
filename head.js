74cc61f07ee19b58965f098419b912f2a4571291
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-11-22 22:51:38 -0500

    Added head.js

---
+++ head.js
@@ -0,0 +1,30 @@
+// This function should not return the first element as an array. It simply returns the element itself.
+
+const head = function(inputArr) {
+  inputArr.slice(1);
+
+  if (inputArr.length >= 1) {
+    return inputArr[0];
+  } else if (inputArr.length === '') {
+    return 'undefined';
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`🙌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🙏 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// Here are some test assertions for you to implement and make pass:
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// Here are some examples how they should be handled:
+// 1)  An array with only one element should still yield that one element as its head
+assertEqual(head(['hi']), 'hi');
+// 2)  An empty array should yield undefined as its head
+assertEqual(head([]),);
