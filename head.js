35620832f514235e29e935d14ec6f14c4b6f5b28
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-11-29 23:17:48 -0500

    Refractored files to only include its function definition, added required files, and exported the the function

--- head.js
+++ head.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 // This function retrieves the first element from the array, often referred to as the "head" of the array. This function should not return the first element as an array. It simply returns the element itself.
 
 
--- middle.js
+++ middle.js
@@ -1,29 +1,7 @@
-/*  This middle function will take in an array and return the middle-most element(s) of the given array. This means that the length of the returned elements could vary.
+// This middle function will take in an array and return the middle-most element(s) of the given array. This means that the length of the returned elements could vary.
 
-For arrays with one or two elements, there is no middle. Return an empty array.
-*/
+// For arrays with one or two elements, there is no middle. Return an empty array.
 
-// Assertion Function
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0, len = arr1.length; i < len; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(a1, a2) {
-
-  if (eqArrays(a1, a2) === true) {
-    return console.log(`🙌 Assertion Passed: ${a1} === ${a2}`);
-  } else {
-    return console.log(`🙏 Assertion Failed: ${a1} !== ${a2}`);
-  }
-};
 
 const middle = function(array) {
   let middleIndex = [];
@@ -43,28 +21,4 @@ const middle = function(array) {
   return middleIndex;
 };
 
-
-// TEST CODEs
-// For arrays with one or two elements, there is no middle; return an empty array.
-console.log(middle([1]));
-console.log(middle([1, 2]));
-// For arrays with odd number of elements, an array containing a single middle element should be returned.
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
-
-
-// TEST ASSERTIONs
-
-assertArraysEqual(middle([]), []);                    // => true thus PASS
-assertArraysEqual(middle([1]), []);                   // => true thus PASS
-assertArraysEqual(middle([1, 2, 3]), [2]);            // => true thus PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);      // => true thus PASS
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);       // => true thus PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); // => true thus PASS
-assertArraysEqual(middle(['Light', 'House', 'Labs']),['Labs']); // => false thus FAIL
-assertArraysEqual(middle(['Light', 'House', 'Labs']),['House']); // => true thus PASS
-assertArraysEqual(middle(['Light', 'House', 'Labs', 'Inc']),['Labs']); // => false thus FAIL
-assertArraysEqual(middle(['Light', 'House', 'Labs','Inc']),['House', 'Labs']); // => true thus PASS
+module.exports = middle;
