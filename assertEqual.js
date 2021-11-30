25caf2467b991d042544d67779c86aa91660f87f
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-11-29 19:34:46 -0500

    messageRefractored, removed test codes and exported the function assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,6 @@
 // If the values match, it should print (console.log) the following:
 // Assertion Passed: [actual] === [expected] (but with the values filled in).  Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
 
-
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
--- head.js
+++ head.js
@@ -1,4 +1,7 @@
+const assertEqual = require('./assertEqual');
+
 // This function retrieves the first element from the array, often referred to as the "head" of the array. This function should not return the first element as an array. It simply returns the element itself.
+
 
 const head = function(inputArr) {
   inputArr.slice(1);
@@ -10,21 +13,4 @@ const head = function(inputArr) {
   }
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`🙌 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🙏 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-// Here are some test assertions for you to implement and make pass:
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-// Here are some examples how they should be handled:
-// 1)  An array with only one element should still yield that one element as its head
-assertEqual(head(['hi']), 'hi');
-// 2)  An empty array should yield undefined as its head
-assertEqual(head([]),);
+module.exports = head;
