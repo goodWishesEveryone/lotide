3ccb769e4085675b6c9ab2431e0c332fdaf16473
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-11-30 00:36:16 -0500

    Converted to use Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,14 +1,23 @@
-// test/headTest.js
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 'Hello' for '[Hello, Lighthouse, Labs]'", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
 
-// Here are some test assertions for you to implement and make pass:
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
 
-// Here are some examples how they should be handled:
-// 1)  An array with only one element should still yield that one element as its head
-assertEqual(head(['hi']), 'hi');
-// 2)  An empty array should yield undefined as its head
-assertEqual(head([]),);
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+
+  it("returns 'undefined' for [], an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
