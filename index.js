e327b1a8eceded8b0a8ef4d30576129c728bbff5
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-11-30 02:43:31 -0500

    Modified index.js to utility functions in Lotide library

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // index.js
-const head              = require('./head');
-const tail              = require('./tail');
-const middle            = require('./middle');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual       = require('./assertEqual');
-const countLetters      = require('./countLetters');
-const countOnly         = require('./countOnly');
-const eqArrays          = require('./eqArrays');
-const letterPositions   = require('./letterPositions');
-const map               = require('./map');
-const without           = require('./without');
-const findKeyByValue    = require('./findKeyByValue');
-const findKey           = require('./findKey');
-const takeUntil           = require('./takeUntil');
+const head               = require('./head');
+const tail               = require('./tail');
+const middle             = require('./middle');
+const assertArraysEqual  = require('./assertArraysEqual');
+const assertEqual        = require('./assertEqual');
+const countLetters       = require('./countLetters');
+const countOnly          = require('./countOnly');
+const eqArrays           = require('./eqArrays');
+const letterPositions    = require('./letterPositions');
+const map                = require('./map');
+const without            = require('./without');
+const findKeyByValue     = require('./findKeyByValue');
+const findKey            = require('./findKey');
+const takeUntil          = require('./takeUntil');
+const eqObjects          = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
 
 module.exports = {
   head              : head,
@@ -28,5 +30,7 @@ module.exports = {
   without           : without,
   findKeyByValue    : findKeyByValue,
   findKey           : findKey,
-  takeUntil         : takeUntil
+  takeUntil         : takeUntil,
+  eqObjects         : eqObjects,
+  assertObjectsEqual:assertObjectsEqual
 };
