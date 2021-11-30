f30e1a352beda95a87f533d388105c0fa2d8b587
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-11-29 19:28:20 -0500

    Refractored: removed the function definition for assertEqual from this file and test codes

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,8 @@
-/*
-This function compares two values it takes in and print out a message if they match or not.
+// This function compares two values it takes in and print out a message if they match or not.
 
-If the values match, it should print (console.log) the following:
-Assertion Passed: [actual] === [expected] (but with the values filled in)
-Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
-*/
+// If the values match, it should print (console.log) the following:
+// Assertion Passed: [actual] === [expected] (but with the values filled in).  Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
+
 
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
@@ -15,14 +13,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-// Test at least the following scenarios:
-// Comparing identical strings
-// Comparing non-identical strings
-// Comparing identical numbers
-// Comparing non-identical numbers
-
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 8);
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Bootcamp");
+module.exports = assertEqual;
