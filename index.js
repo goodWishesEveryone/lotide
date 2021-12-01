3959dc7d1de272fbded1b5fc5d0637e3ad910bbc
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-11-30 22:18:44 -0500

    Modified index.js

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 // index.js
-const head               = require('./head');
-const tail               = require('./tail');
-const middle             = require('./middle');
-const assertArraysEqual  = require('./assertArraysEqual');
-const assertEqual        = require('./assertEqual');
-const countLetters       = require('./countLetters');
-const countOnly          = require('./countOnly');
-const eqArrays           = require('./eqArrays');
-const letterPositions    = require('./letterPositions');
-const map                = require('./map');
-const without            = require('./without');
-const findKeyByValue     = require('./findKeyByValue');
-const findKey            = require('./findKey');
-const takeUntil          = require('./takeUntil');
-const eqObjects          = require('./eqObjects');
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const without = require('./without');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const takeUntil = require('./takeUntil');
+const eqObjects = require('./eqObjects');
 const assertObjectsEqual = require('./assertObjectsEqual');
 
+
 module.exports = {
-  head              : head,
-  tail              : tail,
-  middle            : middle,
-  assertArraysEqual : assertArraysEqual,
-  assertEqual       : assertEqual,
-  countLetters      : countLetters,
-  countOnly         : countOnly,
-  eqArrays          : eqArrays,
-  letterPositions   : letterPositions,
-  map               : map,
-  without           : without,
-  findKeyByValue    : findKeyByValue,
-  findKey           : findKey,
-  takeUntil         : takeUntil,
-  eqObjects         : eqObjects,
-  assertObjectsEqual:assertObjectsEqual
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  letterPositions,
+  map,
+  without,
+  findKeyByValue,
+  findKey,
+  takeUntil,
+  eqObjects,
+  assertObjectsEqual
 };
